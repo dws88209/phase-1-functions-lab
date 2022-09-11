03555e5b8293a736687d17d9c2b5e6cf77094b55
Destinee Saunders <dsaunders290@gmail.com>
2022-09-11 16:05:40 -0400

    commit tree 9decb2a46fe500b9b174c7d3258d69daeeda3e00

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const headquaters= 42
+function distanceFromHqInBlocks(pickup){
+   if (42<pickup) {
+    return pickup - 42
+   } 
+   else {
+    return 42 - pickup
+   } 
+}
+function distanceFromHqInFeet(pickup){
+    distanceFromHqInBlocks(pickup); {
+    if (42<pickup){
+        return ((pickup - 42)*264)
+    }
+    else {
+        return ((42-pickup)*264)
+    }
+    } 
+} 
+function distanceTravelledInFeet(start, destination){
+    if (destination<start){
+    return (start - destination)*264
+} else {
+    return (destination-start)*264
+}
+}
+function calculatesFarePrice(start, destination){
+const distance = distanceTravelledInFeet(start,destination)
+    if (distance <= 400){
+return 0
+} 
+else if (distance>400 && distance<=2000){
+console.log(distance)
+    return (distance-400)*0.02
+} 
+else if (distance>2000 && distance<2500){
+return 25
+} else {
+    return 'cannot travel that far'
+}
+}
